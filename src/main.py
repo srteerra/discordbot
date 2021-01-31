e99284157695c5f357fe5baaceff7bbbb207b34c
Angel Lopez <srterradev@gmail.com>
2021-01-31 15:09:52 -0700

    First command added and also Streaming status

--- src/main.py
+++ src/main.py
@@ -1,4 +1,16 @@
 import discord
 from discord.ext import commands
 
-commands.Bot(command_prefix='#',description="This is a Terra Bot")
+bot = commands.Bot(command_prefix='#',description="This is a Terra Bot")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+# Events
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Streaming(name="Tutorials", url="https://www.twitch.tv/juansguarnizo"))
+    print("My bot is ready")
+
+bot.run('ODA1NTU1ODEzNzU0MjA4Mjc4.YBcmUA.bNp3zH9AQ1IcpPUk1kq_ZpbZJ9U')
